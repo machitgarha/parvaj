3c5d11c9724ef64868473d9e3754e8722606ee20
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-28 00:37:14 +0430

    Add missing namespaces

--- src/Command/CreateEntityCommand.php
+++ src/Command/CreateEntityCommand.php
@@ -2,10 +2,13 @@
 
 namespace MAChitgarha\Parvaj\Command;
 
-use \Symfony\Component\Console\Command\Command;
-use \Symfony\Component\Console\Input\InputArgument;
-use \Symfony\Component\Console\Input\InputInterface;
-use \Symfony\Component\Console\Output\OutputInterface;
+use MAChitgarha\Parvaj\SourceEntityFileCreator;
+use MAChitgarha\Parvaj\UnitTestEntityFileCreator;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class CreateEntityCommand extends Command
 {
