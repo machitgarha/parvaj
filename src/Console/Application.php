13a9d8f965623aad74b0c7ef9c5c6173b3bc2b3e
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-08-10 03:48:34 +0430

    Create new Application class, use it in bin/parvaj

---
+++ src/Console/Application.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MAChitgarha\Parvaj\Console;
+
+use MAChitgarha\Parvaj\Command\CreateEntityCommand;
+use MAChitgarha\Parvaj\Command\RunUnitTestCommand;
+
+class Application extends \Symfony\Component\Console\Application
+{
+    public const NAME = 'Parvaj';
+    public const VERSION = '0.2.0';
+
+    public function __construct()
+    {
+        parent::__construct(self::NAME, self::VERSION);
+
+        $this->addAllCommands();
+    }
+
+    private function addAllCommands()
+    {
+        $this->addCommands([
+            new CreateEntityCommand(),
+            new RunUnitTestCommand(),
+        ]);
+    }
+};
