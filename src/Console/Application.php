abb73a726ff06cd7a4f349dd54d6233db2dc1fb1
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-08-11 15:50:01 +0430

    Bump version to 0.2.0-alpha.1

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -8,7 +8,7 @@ use MAChitgarha\Parvaj\Command\RunUnitTestCommand;
 class Application extends \Symfony\Component\Console\Application
 {
     public const NAME = 'Parvaj';
-    public const VERSION = '0.2.0';
+    public const VERSION = '0.2.0-alpha.1';
 
     public function __construct()
     {
