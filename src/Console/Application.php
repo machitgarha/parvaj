a35cf85cfe95810ca3ce7d2408795b70e40f6437
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-03-29 00:30:36 +0430

    Add composer dependencies before running Phan in CI workflow
    Bump version to 0.3.0-beta.1.

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -7,7 +7,7 @@ use MAChitgarha\Parvaj\Command\SimulateCommand;
 class Application extends \Symfony\Component\Console\Application
 {
     public const NAME = "Parvaj";
-    public const VERSION = "0.2.0";
+    public const VERSION = "0.3.0-beta.1";
 
     public function __construct()
     {
