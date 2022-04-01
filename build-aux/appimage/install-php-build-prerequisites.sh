1671e2f9ffde9f2392e5b114ca03283872842c00
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-04-01 22:23:06 +0430

    Ignore sudo in install-php-build-prerequisites.sh in the case of Docker

--- build-aux/appimage/install-php-build-prerequisites.sh
+++ build-aux/appimage/install-php-build-prerequisites.sh
@@ -3,4 +3,11 @@
 # This script runs on Fedora particularly, so use docker to run on other Linux
 # distributions as well
 
-sudo dnf install -y gcc g++ libxml2-devel libicu-devel oniguruma-devel
+# Don't use sudo for environments without it (like Docker)
+if command -v sudo &> /dev/null; then
+    sudo="sudo"
+else
+    sudo=""
+fi
+
+$sudo dnf install -y gcc g++ libxml2-devel libicu-devel oniguruma-devel
