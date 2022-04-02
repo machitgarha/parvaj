b79d0c2d60acb495defccdf3b7cf5be4a300e8fd
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-04-02 19:18:39 +0430

    Fix library versions when bundling PHP libraries in a Docker container

--- build-aux/appimage/build.sh
+++ build-aux/appimage/build.sh
@@ -99,9 +99,9 @@ bundlePhpSharedLibraries() {
 
     # 64-bit libraries
     for i in \
-        "libcrypt.so.?" libstdc++.so.6 libxml2.so.2 libz.so.1 libicuio.so.69 \
-        libicui18n.so.69 libicuuc.so.69 libicudata.so.69 libonig.so.5 \
-        libgcc_s.so.1 liblzma.so.5 \
+        "libcrypt.so.*" "libstdc++.so.*" "libxml2.so.*" "libz.so.*" \
+        "libicuio.so.*" "libicui18n.so.*" "libicuuc.so.*" "libicudata.so.*" \
+        "libonig.so.*" "libgcc_s.so.*" "liblzma.so.*" \
     ; do
         cp /lib64/$i "$appDir/usr/lib64/"
     done
