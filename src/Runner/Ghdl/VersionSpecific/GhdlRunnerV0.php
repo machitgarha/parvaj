368c4ae0cd9ed55795f2dd8f40f788750e262543
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2023-04-14 15:19:34 +0330

    Improve GHDL version 3 support
    Use -o option only for GHDL version 1.

--- src/Runner/Ghdl/VersionSpecific/GhdlRunnerV0.php
+++ src/Runner/Ghdl/VersionSpecific/GhdlRunnerV0.php
@@ -6,11 +6,4 @@ use MAChitgarha\Parvaj\Runner\Ghdl\GhdlRunner;
 
 class GhdlRunnerV0 extends GhdlRunner
 {
-    protected function getElabRunGeneralOptions(string $testEntityName): array
-    {
-        $result = parent::getElabRunGeneralOptions($testEntityName);
-        unset($result["o"]);
-
-        return $result;
-    }
 }
